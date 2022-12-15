8b602c1c8df892954db09c0d904d13097df6aecd
ALRIZKI HAIKAL <115471452+sipalingtestnet@users.noreply.github.com>
2022-12-15 12:03:53 +0800

    Update Staking.vue

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="❤️ Stake with JambulMerah⚛️  ❤️"
+      title="❤️ Stake with Sipaling-Testnet⚛️  ❤️"
       class="overflow-auto"
     >
       <b-table
