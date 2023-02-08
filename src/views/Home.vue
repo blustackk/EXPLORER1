89478b7eaccadb6282ffc65f72ae3fc006aea6cd
blustackk <106958645+blustackk@users.noreply.github.com>
2023-02-08 23:11:05 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          Explorer.SIPALING TESTNET<small class="flow-left">.dev</small>
+          Explorer.blustackkk<small class="flow-left">.dev</small>
         </h1>
       </div>
     </b-link>
